103c37b1faa5d2213e3e8f380f4d8aa48ec06dcb
Sandeep Dasgupta <sdasgup3@illinois.edu>
2015-09-28 16:42:54 -0500

    isUnique

---
+++ Array/isUnique.cpp
@@ -0,0 +1,18 @@
+/*To implement if a string has all unisue characters.
+ * What if cannot use additional data structures?
+ */
+#include<iostream>
+#include<vector>
+#include<assert.h>
+
+using namespace std;
+
+
+int main() 
+{
+  vector<int> vi(256);
+  std::cout << vi;
+
+
+  return 0;
+}
