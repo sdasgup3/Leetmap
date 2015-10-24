46656ebbaf9e34f65d780d7f0b3f87197c0aaafb
Sandeep Dasgupta <sdasgup3@illinois.edu>
2015-10-24 17:25:30 -0500

    "considered all neg case"

--- Array/maxSumContiguousArray.cpp
+++ Array/maxSumContiguousArray.cpp
@@ -1,8 +1,32 @@
 
 #include<iostream>
 
+void 
+print(int arr[], int n) {
+  for(int i = 0 ; i < n ; i ++ ) {
+    std::cout << arr[i] << " " ;
+  }
+}
+
 int
 maxS(int arr[], int n) {
+  print(arr,n);
+
+  bool allneg = true;
+  for(int i = 0 ; i  < n ; i ++ ) {
+    if(arr[i] >= 0) {
+      allneg = false;
+    }
+  }
+
+  if(allneg) {
+    int max = arr[0];
+    for(int i = 0 ; i  < n ; i ++ ) {
+      max = std::max(max, arr[i]);
+    }
+    return max;
+  }
+
   int max1 = 0; int max2 = 0;
 
   for(int i = 0 ; i  < n ; i ++ ) {
@@ -23,39 +47,33 @@ maxS(int arr[], int n) {
   return max1;
 }
 
-void 
-print(int arr[], int n) {
-  for(int i = 0 ; i < n ; i ++ ) {
-    std::cout << arr[i] << " " ;
-  }
-}
 
 int main() {
 
   int arr[] = {-2,0,-3,4,5, 1, -2};
   int n= sizeof(arr)/sizeof(arr[0]);
-  print(arr, n);
   std::cout << "\nMaxSum: " << maxS(arr, n) ;
   std::cout << " \n" ;
 
   int arr1[] = {1,2,3,4};
   n = sizeof(arr1)/sizeof(arr1[0]);
-  print(arr1, n);
   std::cout << "\nMaxSum: " << maxS(arr1, n) ;
   std::cout << " \n" ;
 
 
   int arr2[] = {2,3,-2,-3};
   n = sizeof(arr2)/sizeof(arr2[0]);
-  print(arr2, n);
   std::cout << "\nMaxSum: " << maxS(arr2, n) ;
   std::cout << " \n" ;
 
   int arr3[] = {-1,5,100,-1000};
   n = sizeof(arr3)/sizeof(arr3[0]);
-  print(arr3, n);
   std::cout << "\nMaxSum: " << maxS(arr3, n) ;
   std::cout << " \n" ;
 
+  int arr4[] = {-5,-100,-1};
+  n = sizeof(arr4)/sizeof(arr4[0]);
+  std::cout << "\nMaxSum: " << maxS(arr4, n) ;
+  std::cout << " \n" ;
   return 0;
 }
