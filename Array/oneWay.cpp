389587abb225007a0d48263093c0555970446ced
Sandeep Dasgupta <sdasgup3@illinois.edu>
2015-10-25 01:52:46 -0500

    "one way v2"

--- Array/oneWay.cpp
+++ Array/oneWay.cpp
@@ -49,31 +49,104 @@ bool oneAway(std::string str1, std::string str2){
   return true;
 }
 
+bool oneAway_v2(std::string str1, std::string str2){
+  int len1 = str1.length();
+  int len2 = str2.length();
+
+  if(!(len1 == len2 || len1 == len2-1 || len2 == len1-1)) {
+    return false;
+  }
+
+
+  bool oneEdit = false ;
+  if(len1 == len2) {
+    for(int i = 0 ; i < len1 ; i++) {
+      if(str1[i] != str2[i]) {
+        if(false == oneEdit) {
+          oneEdit = true;
+        } else {
+          return false;
+        }
+      }
+    }
+  } else {
+    int i = 0, j = 0;
+    for(; i < len1, j < len2 ; ) {
+      if(str1[i] != str2[j]) {
+        if(false == oneEdit) {
+          oneEdit = true;
+        } else {
+          return false;
+        }
+        if(len1 > len2) {
+          i++;
+        } else {
+          j++;
+        }
+      } else{
+        i++;j++;
+      }
+    }
+
+    if(true == oneEdit && (i != len1 || j != len2)) {
+      return false;
+    }
+    //redundant as oneEdit ==true means the are same for the common length part
+    // but not for the extra's which is only one as |len1-len2| == 1
+    if(false == oneEdit ) {
+      if(i == len1 && j != len2 && std::abs(len2-1-j) > 1) {
+        return false;
+      }
+      if(j == len2 && i != len1 && std::abs(len1-1-i) > 1) {
+        return false;
+      }
+    }
+
+  }
+
+  return true;
+}
 int main()
 {
   std::string str1("pale");
   std::string str2("ple");
-  bool res = oneAway(str1, str2);
+  bool res ;
   std::cout << " Str1: "  << str1 << " " << "Str2: " << str2 << std::endl;
+  res = oneAway(str1, str2);
+  std::cout << " oneAway: " << res << std::endl;
+  res = oneAway_v2(str1, str2);
   std::cout << " oneAway: " << res << std::endl;
 
   str1 = "pale";
   str2 = "bake";
-  res = oneAway(str1, str2);
   std::cout << " Str1: "  << str1 << " " << "Str2: " << str2 << std::endl;
+  res = oneAway(str1, str2);
+  std::cout << " oneAway: " << res << std::endl;
+  res = oneAway_v2(str1, str2);
   std::cout << " oneAway: " << res << std::endl;
 
   str1 = "pales";
   str2 = "pale";
-  res = oneAway(str1, str2);
   std::cout << " Str1: "  << str1 << " " << "Str2: " << str2 << std::endl;
+  res = oneAway(str1, str2);
+  std::cout << " oneAway: " << res << std::endl;
+  res = oneAway_v2(str1, str2);
   std::cout << " oneAway: " << res << std::endl;
 
   str1 = "pales";
   str2 = "bales";
-  res = oneAway(str1, str2);
   std::cout << " Str1: "  << str1 << " " << "Str2: " << str2 << std::endl;
+  res = oneAway(str1, str2);
+  std::cout << " oneAway: " << res << std::endl;
+  res = oneAway_v2(str1, str2);
   std::cout << " oneAway: " << res << std::endl;
 
+  str1 = "pales";
+  str2 = "patesxz";
+  std::cout << " Str1: "  << str1 << " " << "Str2: " << str2 << std::endl;
+  res = oneAway(str1, str2);
+  std::cout << " oneAway: " << res << std::endl;
+  res = oneAway_v2(str1, str2);
+  std::cout << " oneAway: " << res << std::endl;
   return 0;
 }
