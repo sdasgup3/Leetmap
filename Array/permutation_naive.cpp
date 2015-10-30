d28f0f66b2aa3f6379f3d186304547893b444de4
Sandeep Dasgupta <sdasgup3@illinois.edu>
2015-10-30 02:33:52 -0500

    "permutation_naive.cpp"

--- Array/permutation_naive.cpp
+++ Array/permutation_naive.cpp
@@ -37,18 +37,49 @@ void fill(int pos, vector<int> exclude, int n, int r)
 }
 
 
-void permute(int n, int r) 
+void permute_v(int n, int r) 
 {
   vector<int> exclude;
   fill(0, exclude, n, r);
 
 }
 
+void permute_helper(std::string str, int &r, std::string &ans, int step) {
+  if(step == r) {
+    std::cout << ans << "\n";
+    Count ++;
+    return;
+  }
+
+  for(int i = 0; i < str.length(); i++  ) {
+    ans.push_back(str[i]);
+
+    std::string remain;
+    if(i == 0) {
+      remain.append(str.begin() + 1, str.end());
+    } else {
+      remain.append(str.begin(), str.begin() + i);
+      remain.append(str.begin()+(i+1), str.end());
+    }
+
+    permute_helper(remain, r, ans, step+1);
+    ans.pop_back();
+  }
+
+
+}
+
+void permute(std::string str, int r) {
+  std::string ans;
+  permute_helper(str, r, ans, 0); 
+}
+
+
 int main() {
-  int n = 5;
-  int r = 3;
-  cout << " n = " << n << " r = " << r << endl;
-  permute(n,r);
+  std::string str("12345");
+  int r = 4;
+  cout << str << " r = " << r << endl;
+  permute(str,r);
   cout << " Number Of Soln: " << Count << endl;
 
   return 0;
