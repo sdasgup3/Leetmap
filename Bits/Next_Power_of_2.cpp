d0260db8eca71d2ab820e4cb7427dae8aa3a9c5d
Sandeep Dasgupta <sdasgup3@illinois.edu>
2015-11-17 13:43:43 -0600

    "other version"

--- Bits/Next_Power_of_2.cpp
+++ Bits/Next_Power_of_2.cpp
@@ -34,26 +34,65 @@ NP2(int x)
     }
   }
 
-  for(int i = 0 ;i <= last_1; i++) {
-    v[i] = 0;
-  }
-  v[last_1+1] =1;
-
-
-  int sum = 0, i = 0;
-  while(i < v.size()) {
-    sum += (v[i] << i);
-    i++;
-  }
-
-  return sum;
+  std::cout << last_1 << "\n";
+  return (int)std::pow(2, last_1+1);
 }
 
+int 
+NP1(int x)
+{
+  if(0 == x) {
+    return 1;
+  }
+
+  if(0 != x && 0 == (x & (x-1)) ) {
+    return x;
+  }
+
+  int last_1=0;
+  while(x) {
+    x = x/2;
+    last_1++;
+  }
+
+  std::cout << last_1 << "\n";
+  return (int)std::pow(2, last_1);
+}
+
+int 
+NP3(int x)
+{
+  if(0 == x) {
+    return 1;
+  }
+
+  if(0 != x && 0 == (x & (x-1)) ) {
+    return x;
+  }
+
+  int last_1 = std::ceil(std::log(x)/std::log(2));
+  std::cout << last_1 << "\n";
+  return (int)std::pow(2, last_1);
+}
+
+unsigned int 
+NP4(unsigned int n)
+{
+    unsigned int p = 1;
+    if (n && !(n & (n - 1)))
+        return n;
+ 
+    while (p < n) {
+        p <<= 1;
+    }
+    return p;
+}
+ 
 
 int main() {
   for(int i = 0 ; i < 50; i++) {
-    std::cout << i << " " << NP2(i) << "\n";
+    std::cout << i << " " << NP4(i) << "\n";
   }
-  std::cout << -1 << " " << NP2(-1) << "\n";
+  std::cout << -1 << " " << NP4(-1) << "\n";
 }
 
