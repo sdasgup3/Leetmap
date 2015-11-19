8fdb711049e3b5b7ed12341287a1a235a7f6e817
Sandeep Dasgupta <sdasgup3@illinois.edu>
2015-11-19 16:56:28 -0600

    "binRep.cpp"

--- G4G/Bit/binRep.cpp
+++ G4G/Bit/binRep.cpp
@@ -12,7 +12,7 @@ printBR(int  n, std::vector<int> V) {
 }
 
 void
-BR(int n) {
+BR_1(int n) {
   int size = 8*sizeof(int);
   std::vector<int> V(size, 0);
 
@@ -37,11 +37,55 @@ BR(int n) {
 
 }
 
+void
+BR_2(int n) {
+  int size = 8*sizeof(int);
+  int mask  = std::pow(2, size-1);
+
+  std::cout<< n << " : " ;
+  for(; mask; mask = ((unsigned int)mask >> 1)) {
+    if(n&mask) {
+      std::cout << "1 ";
+    } else {
+      std::cout << "0 ";
+    }
+  }
+    std::cout << "\n " ;
+}
+
+void
+BR_3H(int n) {
+  if(0 == n) 
+    return;
+
+  int tp = n&1;
+  BR_3H(n/2);
+  std::cout << tp << " ";
+}
+
+void
+BR_3(int n) {
+  std::cout<< n << " : " ;
+  int size = 8*sizeof(int);
+
+  if(0 == n) {
+    std::cout << "0 ";
+  } else {
+    if(n< 0) {
+      n = std::pow(2, size-1) + n;
+      std::cout << "1 ";
+    }
+    BR_3H(n);
+  }
+  std::cout << "\n " ;
+}
 
 int 
 main()
 {
   for(int i = -8;  i <= 7 ; i++) {
-    BR(i);
+    BR_1(i);
+    BR_2(i);
+    BR_3(i);
   }
 }
