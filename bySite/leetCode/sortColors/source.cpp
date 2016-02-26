b5e326e2b4027fdee0a7fe88291c6065a6533bfc
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-02-25 21:13:43 -0600

    sort colors

---
+++ bySite/leetCode/sortColors/source.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    void partition(vector<int>& nums, int pivot) {
+        int size = nums.size();
+        int first = 0;
+        int last = size-1;
+        
+        while(first < last) {
+            while(first < size && nums[first] <= pivot) {
+                first++;
+            }
+            
+            while(last >= 0 && nums[last] > pivot) {
+                last --;
+            }
+            
+            if(first < last) {
+                int temp = nums[first];
+                nums[first] = nums[last];
+                nums[last] = temp;
+            }
+        }
+    }
+    void sortColors(vector<int>& nums) {
+        partition(nums, 0);
+        partition(nums,1);
+    }
+};
+
+int main() {
+	Solution S;
+	int arr[] = {2,1,0};
+	std::vector<int> V(arr, (arr + sizeof(arr)/sizeof(arr[0])) );
+	S.sortColors(V);
+
+}
