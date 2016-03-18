5124c259ab1863fa50c05c707838d53f6144b38b
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-03-17 19:55:28 -0500

    addDigits

--- bySite/leetCode/addDigits/source.cpp
+++ bySite/leetCode/addDigits/source.cpp
@@ -1,16 +1,9 @@
-class Solution {
-public:
-int addDigits1(int num) {
-        
-            return (num-1)%9 +1;
-        
-    }
-    int addDigits(int num) {
-        if(num <= 9) {
+int Solution::func(int num) {
+    //   return (num-1)%9 +1;
+     if(num <= 9) {
             return num;
         } else {
             return (num-10)%9 +1;
         }
         
-    }
-};
+}
