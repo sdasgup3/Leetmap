dd424bd2fbfdf7027cff1c099ad539617d8cf153
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-09-05 17:58:55 -0500

    5 Sept 2016

--- source/leetcode/Count_Primes.cpp
+++ source/leetcode/Count_Primes.cpp
@@ -1,5 +1,13 @@
 /* Problem Description
  * Count the number of prime numbers less than a non-negative number, n.
+Let's say m = sqrt(n) then m × m = n. Now if n is not a prime then n can be written as n = a × b, so m × m = a × b. Notice that m is a real number whereas n, a and b are natural numbers.
+
+Now there can be 3 cases:
+
+a > m ⇒ b < m
+a = m ⇒ b = m
+a < m ⇒ b > m
+In all 3 cases, min(a, b) ≤ m. Hence if we search till m, we are bound to find at least one factor of n, which is enough to show that n is not prime.
 */
 
 #include"header.h"
